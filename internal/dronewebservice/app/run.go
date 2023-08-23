e6db34e8909d6fc8fb78e4fe2b1ed418088ce5bc
vicislave <slave@altione.altione.net>
2023-08-23 20:39:45 +0300

    fixed bugs

--- internal/dronewebservice/app/run.go
+++ internal/dronewebservice/app/run.go
@@ -3,5 +3,5 @@ package app
 func (a *App) Run() {
 	a.log.Info("DroneWebService is starting handle")
 	go a.handler.StartHandle(a.cfg.HTTPHost, a.cfg.HTTPPort)
-	go a.handler.StartEnpointUdpServerHandle("127.0.0.1", "5600", a.cfg.Dialect, 10)
+	// go a.handler.StartEnpointUdpServerHandle("127.0.0.1", "5600", a.cfg.Dialect, 10)
 }
